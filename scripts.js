c2f76b513b57d03177adcbefc695a8450f415a1b
MirelaLira <mirela_s@hotmail.com>
2018-10-22 22:31:39 -0300

    Continuação do exercício

--- scripts.js
+++ scripts.js
@@ -1,43 +1,12 @@
+const linkCriarUmaConta = <a class= "link" href="/conta">Criar uma conta</a>
 
-// // <a class="link" href="/conta">Criar uma conta</a>
+const fazerLogin = <a class= "link" href="/login">Fazer Login</a>
 
-// const linkCriarUmaConta = document.createElement('a')
-// linkCriarUmaConta.className = 'link'
-// linkCriarUmaConta.href = '/conta'
-// linkCriarUmaConta.appendChild(document.createTextNode('Criar uma conta'))
-
-
-const linkCriarUmaConta = React.createElement ('a', {
-    className: 'link',
-    herf: '/conta',
-    children: 'criar uma conta'
-    
-})
-
-const fazerLogin = React.createElement ('a', {
-    className: 'link',
-    herf: '/conta',
-    children: 'fazer login'
-    
-})
-
-const formularioLogin = React.createElement('form',{
-    children: [linkCriarUmaConta, fazerLogin]
-})
+const formularioLogin = (
+    <form>
+        {[linkCriarUmaConta, fazerLogin]}
+        </form>
+)
 const divisaoProjeto = document.getElementById('projeto')
+
 ReactDOM.render(formularioLogin, divisaoProjeto)
-
-// const linkFazerLogin = document.createElement('a')
-// linkFazerLogin.className = 'link'
-// linkFazerLogin.href = '/conta'
-// linkFazerLogin.appendChild(document.createTextNode('Fazer uma conta'))
-
-// const formularioLogin = document.createElement('form')
-// formularioLogin.appendChild(linkCriarUmaConta)
-// formularioLogin.appendChild(linkFazerLogin)
-
-
-// const divisaoProjeto = document.getElementById('projeto')
-// divisaoProjeto.appendChild(formularioLogin)
-
-
