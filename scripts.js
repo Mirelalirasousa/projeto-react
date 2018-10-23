551458ec6d8292713e4a0aec9cf812b5f21554f3
MirelaLira <mirela_s@hotmail.com>
2018-10-22 22:04:18 -0300

    Exercício Feito com React

--- scripts.js
+++ scripts.js
@@ -1,25 +1,43 @@
 
-// <a class="link" href="/conta">Criar uma conta</a>
+// // <a class="link" href="/conta">Criar uma conta</a>
 
-const linkCriarUmaConta = document.createElement('a')
-linkCriarUmaConta.className = 'link'
-linkCriarUmaConta.href = '/conta'
-linkCriarUmaConta.appendChild(document.createTextNode('Criar uma conta'))
-
-
-const 
-
-const linkFazerLogin = document.createElement('a')
-linkFazerLogin.className = 'link'
-linkFazerLogin.href = '/conta'
-linkFazerLogin.appendChild(document.createTextNode('Fazer uma conta'))
-
-const formularioLogin = document.createElement('form')
-formularioLogin.appendChild(linkCriarUmaConta)
-formularioLogin.appendChild(linkFazerLogin)
+// const linkCriarUmaConta = document.createElement('a')
+// linkCriarUmaConta.className = 'link'
+// linkCriarUmaConta.href = '/conta'
+// linkCriarUmaConta.appendChild(document.createTextNode('Criar uma conta'))
 
 
+const linkCriarUmaConta = React.createElement ('a', {
+    className: 'link',
+    herf: '/conta',
+    children: 'criar uma conta'
+    
+})
+
+const fazerLogin = React.createElement ('a', {
+    className: 'link',
+    herf: '/conta',
+    children: 'fazer login'
+    
+})
+
+const formularioLogin = React.createElement('form',{
+    children: [linkCriarUmaConta, fazerLogin]
+})
 const divisaoProjeto = document.getElementById('projeto')
-divisaoProjeto.appendChild(formularioLogin)
+ReactDOM.render(formularioLogin, divisaoProjeto)
+
+// const linkFazerLogin = document.createElement('a')
+// linkFazerLogin.className = 'link'
+// linkFazerLogin.href = '/conta'
+// linkFazerLogin.appendChild(document.createTextNode('Fazer uma conta'))
+
+// const formularioLogin = document.createElement('form')
+// formularioLogin.appendChild(linkCriarUmaConta)
+// formularioLogin.appendChild(linkFazerLogin)
+
+
+// const divisaoProjeto = document.getElementById('projeto')
+// divisaoProjeto.appendChild(formularioLogin)
 
 
