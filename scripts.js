d883eb422984a2b36df8a97620e6ed39e19a9d5d
Mirela Lira <mirela_s@hotmail.com>
2018-10-23 21:26:32 -0300

    Inserir botão desabilitado

--- scripts.js
+++ scripts.js
@@ -1,12 +1,46 @@
-const linkCriarUmaConta = <a class= "link" href="/conta">Criar uma conta</a>
+function Link(props) {
+    return (
+        <a className="link" href={props.url}>
+            {props.children}
+        </a>
+    )
+}
 
-const fazerLogin = <a class= "link" href="/login">Fazer Login</a>
+// function Botao(props){
+//     return(
+//         <a className="botao" href={props.url}>
+//          {props.children}
+//         </a>
 
-const formularioLogin = (
-    <form>
-        {[linkCriarUmaConta, fazerLogin]}
-        </form>
+//     )
+// }
+
+function Botao(props){
+    let classes = "botao"
+
+    if (props.desabilitado){
+        classes += " botao--desabilitado"
+    }
+
+    return(
+        <input className={classes} type="submit" value={props.children}/>
+    )
+}
+
+const paginaLogin = (
+    <main className="pagina-login">
+        <h1>Login</h1>
+        <p>Entre com seu email e senha.</p>
+        <Link url='/conta'>Criar uma conta</Link>
+        <Link url='/login'>Fazer login</Link>
+        {/* <Botao className="botao">Enviar</Botao>
+        {/* <Botao className="botao-warning"></Botao> */}
+        <Botao desabilitado={true}>Enviar</Botao>
+        <Botao>Enviar</Botao>
+
+    </main>
 )
-const divisaoProjeto = document.getElementById('projeto')
+// const props = {url:'/conta', children: 'Criar uma conta' }
 
-ReactDOM.render(formularioLogin, divisaoProjeto)
+const divisaoProjeto = document.getElementById('projeto')
+ReactDOM.render(paginaLogin, divisaoProjeto)
