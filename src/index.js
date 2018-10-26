79d30a21118d41f68ad4830befc0875b30a05f32
MirelaliraSousa <mirela_s@hotmail.com>
2018-10-25 22:33:05 -0300

    Alterar para class

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Login from './paginas/Login/Login'
+import Conta from './paginas/Conta/Conta'
 import './index.css'
-import Login from './paginas/Login/Login';
 
 // const props = {url:'/conta', children: 'Criar uma conta' }
 
 const divisaoProjeto = document.getElementById('projeto')
 
-ReactDOM.render(<Login />, divisaoProjeto)
+ReactDOM.render(<Conta />, divisaoProjeto)
