03c7375b7839645667a5a44a3c7ad80808b10e72
Mirelalirasousa <mirela_s@hotmail.com>
2018-11-07 22:31:15 -0200

    Colocando carregamento

--- src/paginas/Home/Home.js
+++ src/paginas/Home/Home.js
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, { Component} from 'react'
 import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
+import carregando from './carregando.svg'
 import './Home.css'
 
-function Home(props) {
-    if (!props.usuario){
-        return <Redirect to="/login"/>
+
+class Home extends Component {
+    constructor(props){
+        super(props)
+        this.state = {carregando: true}
     }
-    
-    return(
-        <main className="home">
-        </main>
-    )
-}
+    render() {
+        if (!this.props.usuario){
+            return <Redirect to="/login"/>
+        }
         
+        return(
+            <main className="home">
+            {this.state.carregando ? (
+                <img className="home__loading" src={carregando} alt="Carregando"/>
+                ) : (
+                    <div>
+                     Aqui Listarei os postits, amanhã
+                    </div>
+                )}
+            </main>
+        )
+    }                
+}
 export default connect((state) => ({ usuario: state.usuario }))(Home)
